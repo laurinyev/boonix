28bf1bf9c002b2c1ad61c887f0fa8d4b1778c7b1
laurinyev <retek1544@gmail.com>
2026-02-15 11:40:07 +0100

    [RB] simple lexer

--- apps/rebourne/src/main.rs
+++ apps/rebourne/src/main.rs
@@ -2,23 +2,59 @@ use std::{
     env::*,
 };
 
+mod interp {
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    enum LexToken<'a>{
+        Word(&'a str)
+    }
+
+    fn lex<'a>(cmd: &'a str) -> Vec<LexToken<'a>> {
+        let mut tokens: Vec<LexToken> = vec![]; 
+        let mut word_start_idx = None;
+
+        for (i,c) in cmd.chars().enumerate() {
+            if word_start_idx.is_some() && c.is_whitespace() {
+                tokens.push(LexToken::Word(&cmd[word_start_idx.unwrap()..i])); 
+                word_start_idx = None;
+            }
+
+            if c.is_alphabetic() && word_start_idx.is_none() {
+                word_start_idx = Some(i);
+            } 
+        };
+        return tokens;
+    }
+
+    pub fn run(cmd: &str) -> u32 {
+        println!("{:?}",lex(cmd));
+
+        return 0;
+    }    
+}
+
 mod repl {
-    use std::{
-        env::*, io::*
+    
+    use {
+        std::{
+            env::*, 
+            io::*
+        },
+        super::interp
     };
 
     fn print_prompt() {
        match var("PS1") {
             Ok(v) => {
-                print!("{} ",v);
+                print!("{}",v);
             },
             Err(..) => {
                 match var("PWD") {
                     Ok(v) => {
-                        print!("{}> ",v);
+                        print!("{}>",v);
                     },
                     Err(..) => {
-                        print!("> ");
+                        print!(">");
                     }
                 } 
             }
@@ -31,7 +67,7 @@ mod repl {
             print_prompt();
             let mut instr = String::new(); 
             stdin().read_line(&mut instr).unwrap();
-            print!("{}",instr); 
+            interp::run(instr.as_str());
         }
     }
 }
