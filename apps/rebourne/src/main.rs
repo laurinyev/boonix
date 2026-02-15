dd9ffd19c4884e03e274b7efc3faa5c91f0f050b
laurinyev <retek1544@gmail.com>
2026-02-15 11:07:54 +0100

    [RB] basic shell thingeh

--- apps/rebourne/src/main.rs
+++ apps/rebourne/src/main.rs
@@ -1,3 +1,48 @@
+use std::{
+    env::*,
+};
+
+mod repl {
+    use std::{
+        env::*, io::*
+    };
+
+    fn print_prompt() {
+       match var("PS1") {
+            Ok(v) => {
+                print!("{} ",v);
+            },
+            Err(..) => {
+                match var("PWD") {
+                    Ok(v) => {
+                        print!("{}> ",v);
+                    },
+                    Err(..) => {
+                        print!("> ");
+                    }
+                } 
+            }
+        } 
+        stdout().flush().unwrap();
+    }
+    
+    pub fn run() {
+        loop {
+            print_prompt();
+            let mut instr = String::new(); 
+            stdin().read_line(&mut instr).unwrap();
+            print!("{}",instr); 
+        }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = args();
+
+    if args.len() < 2 {
+        repl::run();
+    } else if args.len() < 4 {
+        println!("script: {}", args.nth(1).unwrap());
+    }
 }
