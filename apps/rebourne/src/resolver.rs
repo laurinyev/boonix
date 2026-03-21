7744aedc955fdc01ad676a3f91cf32cc4431d234
laurinyev <retek1544@gmail.com>
2026-03-21 11:37:08 +0100

    [RB] okay turns out cargo test demands thread safe code

--- apps/rebourne/src/resolver.rs
+++ apps/rebourne/src/resolver.rs
@@ -1,8 +1,10 @@
 
 use std::{
     fs::*,
-    path::*
+    path::*,
+    env::*
 };
+
 const PATH_SEP: &str = if cfg!(any(target_os = "windows", feature = "safaos")) { ";" } else { ":" };
 
 static mut PATHS : Vec<String> = Vec::new();
@@ -18,7 +20,23 @@ pub fn set_path(path: &str) {
 
 #[allow(static_mut_refs)]
 pub fn resolve(util_name: &str) -> Option<String> {
-    for p in unsafe { PATHS.clone() } {
+    // bitchass cargo test expects it to be thread safe and allat 
+    let paths = if cfg!(test) {
+        //we dont give a damn if this one is slow
+        let pathstr = match var("PATH") {
+            Ok(v) => {
+                v
+            },
+            Err(..) => {
+                option_env!("REBOURNE_DEFAULT_PATH").unwrap_or("/usr/bin:/usr/local/bin:/usr/local/sbin").to_string()
+            }
+        };
+        pathstr.to_string().split(PATH_SEP).map(|s| s.to_string()).collect()
+    } else {
+        unsafe { PATHS.clone() }
+    };
+
+    for p in paths {
         if !exists(&p).unwrap_or(false) {
             continue;
         }
--- apps/rebourne/src/tests.rs
+++ apps/rebourne/src/tests.rs
@@ -1,28 +1,22 @@
-use std::{
-    env::*
-};
-
 use super::{
     interp::*,
-    resolver::*
 };
-
-// has to be called every test cuz fck you 
-fn test_init() {
-    match var("PATH") {
-        Ok(v) => {
-            set_path(&v)
-        },
-        Err(..) => {
-            let default_path: &str = option_env!("REBOURNE_DEFAULT_PATH").unwrap_or("/usr/bin:/usr/local/bin:/usr/local/sbin");
-            set_path(default_path)
-        }
-    }
-}
 
 #[test]
 fn basic_echo() {
-    test_init();
-
     assert_eq!(run("echo \"Werx\""), (0, "Werx\n".to_string()));
+}
+
+#[test]
+fn semi_echo() {
+    assert_eq!(run("echo \"Werx\"; echo \"Other part too!\""), (0, "Werx\nOther part too!\n".to_string()));
+}
+
+#[test]
+fn multiline_echo() {
+    assert_eq!(run("echo \"Werx\"\necho \"Other line too!\""), (0, "Werx\nOther line too!\n".to_string()));
+}
+#[test]
+fn looong_semi() {
+    assert_eq!(run("echo \"a\"; echo \"s\"; echo \"d\"; echo \"f\"; echo \"g\"; echo \"h\""), (0, "a\ns\nd\nf\ng\nh\n".to_string()));
 }
